73ab35cd9ad8d0f1e898d017164a491b6a28e03c
Eric BOUILLAUT <e.bouillaut@yahoo.fr>
2023-05-11 15:02:40 +0200

    Correction event click sur medaillon (soutenance)

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -98,7 +98,8 @@ async function init() {
     const y = event.clientY;
 
     // On récupère l'index de l'image cliquée dans les medias
-    if (event.target.tagName === 'IMG' || event.target.tagName === 'VIDEO') {
+    if ((event.target.tagName === 'IMG' || event.target.tagName === 'VIDEO') && event.target.parentElement.className !== 'photograph-header') {
+      console.log(event.target.parentElement.className);
       const photographerMedias = photographer.medias;
       const mediaId = event.target.id;
       const media = photographerMedias.find(media => media.id == mediaId);
